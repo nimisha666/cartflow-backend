0e4f97eb9f37b014bcfd8edbcc3262f2dd7f5849
nishaD2223 <nimishad2223@gmail.com>
2025-02-10 23:38:23 +0530

    bcrypt

--- src/users/user.model.js
+++ src/users/user.model.js
@@ -1,6 +1,6 @@
 
 const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcryptjs");
 
 const userSchema = new Schema({
     username: { type: String, required: true, unique: true },
