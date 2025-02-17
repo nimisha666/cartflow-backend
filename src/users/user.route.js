ffe73c4ebaa7bbcb24db70c6b89825404d0e5dd4
nishaD2223 <nimishad2223@gmail.com>
2025-02-17 18:14:26 +0530

    user

--- src/users/user.route.js
+++ src/users/user.route.js
@@ -13,16 +13,16 @@ router.post('/register', async (req, res) => {
         const existingUser = await User.findOne({ email });
 
         if (existingUser) {
-            return res.status(400).json({ message: "User already exists" });
+            return res.status(400).json({ message: 'User already exists' });
         }
 
         const newUser = new User({ username, email, password });
         await newUser.save();
 
-        res.status(201).json({ message: "User registered successfully" });
+        res.status(201).json({ message: 'User registered successfully' });
     } catch (error) {
-        console.error("Error registering user:", error);
-        res.status(500).json({ message: "Server error" });
+        console.error('Error registering user:', error);
+        res.status(500).json({ message: 'Server error' });
     }
 });
 
